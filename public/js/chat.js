61e380ddf809a92ee52d27127cfbb1340c81f078
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2025-12-02 13:24:40 -0500

    Finalizando chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,49 @@ if (!username) {
 const lblStatusOnline = document.querySelector('#status-online');
 const lblStatusOffline = document.querySelector('#status-offline');
 
+const userUlElement = document.querySelector('.ul');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const chatElement = document.querySelector('#chat');
+
+const renderUsers = (users)=>{
+    userUlElement.innerHTML = '';
+    users.forEach(user => {
+        const liElement = document.createElement('li');
+        liElement.innerText = user.name;
+        userUlElement.appendChild(liElement);
+        
+    });
+}
+
+const renderMessage = (payload)=>{
+    const { userId,name, message } = payload;
+
+    const divElement = document.createElement('div');
+    divElement.classList.add('message');
+
+    if(userId !== socket.id){
+        divElement.classList.add('incoming');
+    }
+
+    divElement.innerHTML = `
+    <small>${name}</small>
+    <p>${message}</p>`;
+    chatElement.appendChild(divElement);
+
+    chatElement.scrollTop = chatElement.scrollHeight;
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const message = input.value;
+    input.value = '';
+
+    socket.emit('send-message', message);
+});
+
 const socket = io({
     auth:{
         token:'ABC-123',
@@ -26,3 +69,12 @@ socket.on('disconnect', () => {
     lblStatusOffline.classList.remove('hidden');
     lblStatusOnline.classList.add('hidden');
 });
+
+
+socket.on('welcome-message',(data)=>{
+    console.log(data);
+} )
+
+socket.on('on-clients-changed', renderUsers)
+
+socket.on('on-message', renderMessage );
--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,4 +1,4 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
 import { OnModuleInit } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
@@ -18,17 +18,51 @@ export class ChatGateway implements OnModuleInit{
   onModuleInit() {
     this.server.on('connection', (socket:Socket) => {
       const { token, name } = socket.handshake.auth;
-      console.log({token, name});
+     
       if(!name){
         socket.disconnect();
         return;
       }
 
+
+      this.chatService.onClientConnected({id: socket.id, name});
+
+
+      // socket.emit('welcome-message','Bienvenido al servidor');
+
+      //Listado de clientes
+      this.server.emit('on-clients-changed', this.chatService.getClients());
+
       socket.on('disconnect', () => {
-        // console.log('Cliente desconectado: ', socket.id);
+        this.chatService.onClientDisconnected(socket.id);
+        this.server.emit('on-clients-changed', this.chatService.getClients());
       });
      
 } );
+  }
+
+
+  @SubscribeMessage('send-message')
+  handleMessage(
+     @MessageBody() message:string,
+     @ConnectedSocket() client:Socket,
+  ){
+   
+    const {name, token} = client.handshake.auth;
+    // console.log({name, message});
+
+    if(!message){
+      return;
+    }
+
+    this.server.emit(
+      'on-message',
+      {
+        userId:client.id,
+        message:message,
+        name:name
+      }
+    )
   }
 }
 
